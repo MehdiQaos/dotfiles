aa30da222d21c90e277450d2df2c62225ca1c4f4
Mehdi Qaos <mehdi.qaos@gmail.com>
2022-06-16 02:24:07 +0100

    added bash

--- zsh/.config/zsh/zsh_plugins_install.sh
+++ zsh/.config/zsh/zsh_plugins_install.sh
@@ -2,7 +2,7 @@
 #stupid script writen(writen?) by me stupid man
 
 plgns="./plgns"
-plugins_dir="$HOME/.local/share/zsh/plugins"
+plugins_dir="$HOME/.local/share/zsh/plugins/"
 github="https://github.com/"
 
 if [[ ! -e $plugins_dir ]]; then
@@ -11,11 +11,10 @@ fi
 
 readarray -t lesplugins < $plgns
 
-cd $plugins_dir
 for plg in ${lesplugins[@]}; do
   array=(${plg//\// })              #split plg by '/' to an array
   folder_name=${array[-1]}          #take the last element
-  if [[ ! -e $folder_name ]]; then
-    git clone $github$plg
+  if [[ ! -e $plugins_dir$folder_name ]]; then
+    git -C $plugins_dir clone $github$plg
   fi
 done
